eeffbef4b6e195b87dbe09223cc69d3c249f06c5
LDJ-creat <2271115019@qq.com>
2025-02-10 20:36:49 +0800

    Delete PisaList directory

--- PisaList/src/types/Task.ts
+++
@@ -1,9 +0,0 @@
-export interface Task {
-  id: string;
-  event: string;
-  completed: boolean;
-  is_cycle: boolean;
-  description: string;
-  importanceLevel: number;
-  completed_date: string;
-} 
--- PisaList/src/utils/envCheck.ts
+++
@@ -1,9 +0,0 @@
-export function checkEnvironment() {
-  const requiredEnvVars = ['VITE_REACT_APP_BASE_URL'];
-  
-  for (const envVar of requiredEnvVars) {
-    if (!import.meta.env[envVar]) {
-      throw new Error(`Missing required environment variable: ${envVar}`);
-    }
-  }
-} 
